8d9b37956aae7d817fa815288663716508e37388
Ryan <ryanjewel05@gmail.com>
2022-05-23 03:41:05 +0530

    ne

--- eww/eww/selvol.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-case $1 in
-  up)
-   amixer -D pipewire set Master 5%+
-   ;;
- down)
-  amixer -D pipewire set Master 5%-;
-esac
--- eww/workspaces.sh
+++ eww/workspaces.sh
@@ -5,13 +5,11 @@ work1=$(herbstclient tag_status | tr -s '[:blank:]' '\n'| sed -n 2p)
 work2=$(herbstclient tag_status | tr -s '[:blank:]' '\n'| sed -n 3p)
 work3=$(herbstclient tag_status | tr -s '[:blank:]' '\n' |sed -n 4p)
 work4=$(herbstclient tag_status | tr -s '[:blank:]' '\n'| sed -n 5p)
-work5=$(herbstclient tag_status | tr -s '[:blank:]' '\n' |sed -n 6p)
 
 wo1=$(herbstclient tag_status | tr -s '[:blank:]' '\n'| grep \: | grep 1)
 wo2=$(herbstclient tag_status | tr -s '[:blank:]' '\n'| grep \: | grep 2)
 wo3=$(herbstclient tag_status | tr -s '[:blank:]' '\n'| grep \: | grep 3)
 wo4=$(herbstclient tag_status | tr -s '[:blank:]' '\n'| grep \: | grep 4)
-wo5=$(herbstclient tag_status | tr -s '[:blank:]' '\n'| grep \: | grep 5)
 
 work_act=$(herbstclient tag_status | tr -s '[:blank:]' '\n'| grep \#)
 
@@ -19,7 +17,6 @@ wc1="empty"
 wc2="empty"
 wc3="empty"
 wc4="empty"
-wc5="empty"
 
     {
 if [ "$work1" == "$work_act" ] ; then
@@ -29,10 +26,8 @@ elif [ "$work2" == "$work_act" ] ; then
     wc2="active_workspace"
 elif [ "$work3" == "$work_act" ] ;   then
     wc3="active_workspace"
-elif [ "$work4" == "$work_act" ] ; then
-    wc4="active_workspace"
 else 
-    wc5="active_workspace"
+    wc4="active_workspace"
 fi
 }
 if [ "$wo1" == ":1" ] ; then
@@ -47,11 +42,8 @@ fi
 if [ "$wo4" == ":4" ]   ; then 
     wc4="occupied_workspace"
 fi    
-if [ "$wo5" == ":5" ] ; then
-wc5="occupied_workspace"
-fi
 
-echo "(box :class \"workspaces\" :orientation \"h\"    :halign \"start\" :spacing \"15\"     :space-evenly \"false\"  (button  :onclick \"herbstclient use 1\"     :class \"$wc1\"     \"♧\" ) (button :onclick \"herbstclient use 2\"  :class \"$wc2\"  \"♢\") (button  :onclick \"herbstclient use 3\"   :class \"$wc3\"  \"/\" ) (button :onclick \"herbstclient use 4 \"  :class \"$wc4\"     \"♡\" ) (button :onclick \"herbstclient use 5\"   :class \"$wc5\"    \"♢\" ))"
+echo "(box :class \"workspaces\" :orientation \"h\"    :halign \"start\" :spacing \"15\"     :space-evenly \"false\"  (button  :onclick \"herbstclient use 1\"     :class \"$wc1\"     \"♣\" ) (button :onclick \"herbstclient use 2\"  :class \"$wc2\"  \"♦\")  (button :onclick \"herbstclient use 3 \"  :class \"$wc3\"     \"♥\" ) (button :onclick \"herbstclient use 4\"   :class \"$wc4\"    \"♠\" ))"
 
 read -r || break
 done
